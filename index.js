2d4cb5229c163b0fd85820a2fa9a5664acfd3581
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2024-01-15 16:20:13 +0400

    added class Task

--- index.js
+++ index.js
@@ -2,3 +2,50 @@
 
 console.clear();
 
+
+
+class Task {
+    #id;
+    #name;
+    #description;
+    #date;
+    #dateInst;
+    #isDone;
+    constructor(name, description) {
+        this.#id = crypto.randomUUID();
+        this.#name = name;
+        this.#description = description;
+        this.#dateInst = new Date();
+        let date = new Date();
+        let dateArr = date.toISOString().split('.')[0].split('T');
+        let dayMonthYear = dateArr[0].split('-').reverse().join('.');
+        let hourMinSec = dateArr[1];
+        this.#date = `${dayMonthYear} ${hourMinSec}`;
+        this.#isDone = false;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get date() {
+        return this.#date;
+    }
+
+    get isDone() {
+        return this.#isDone;
+    }
+
+    get dateInst() {
+        return this.#dateInst;
+    }
+}
+
