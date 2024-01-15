b4097e09cb3a31d3decd2293e9482881449d1835
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2024-01-15 21:18:41 +0400

    added event listener for form to add tasks, fuctiononality to delete button and update function

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 console.clear();
 
-
-
+localStorage.clear();
 class Task {
     #id;
     #name;
@@ -11,12 +10,12 @@ class Task {
     #date;
     #dateInst;
     #isDone;
-    constructor(name, description) {
-        this.#id = crypto.randomUUID();
+    constructor(name, description, isoDate=(new Date()).toISOString()) {
+        this.#id = "id" + Math.random().toString(16).slice(2);
         this.#name = name;
         this.#description = description;
-        this.#dateInst = new Date();
-        let date = new Date();
+        this.#dateInst = new Date(isoDate);
+        let date = new Date(isoDate);
         let dateArr = date.toISOString().split('.')[0].split('T');
         let dayMonthYear = dateArr[0].split('-').reverse().join('.');
         let hourMinSec = dateArr[1];
@@ -47,62 +46,84 @@ class Task {
     get dateInst() {
         return this.#dateInst;
     }
+
+    toJson() {
+        return JSON.stringify({
+            ['#id']: this.#id,
+            ['#name']: this.#name,
+            ['#description']: this.#description,
+            ['#date']: this.#date,
+            ['#dateInst']: this.#dateInst,
+            ['#isDone']: this.#isDone,
+        });
+    }
+
+
 }
 
 class TasksList {
     #tasks;
     #filteredTasks;
+    #filterType = 'all';
 
     constructor(...tasks) {
         this.#tasks = [...tasks];
-        this.#filteredTasks=[...tasks];
+        this.#filteredTasks = [...tasks];
     }
 
-    addTask(task) {
-        this.#tasks.push(task);
+    addTasks(...tasks) {
+        this.#tasks.push(...tasks);
+        this.#filteredTasks.push(...tasks);
     }
-    
+
     sortByDate() {
-        this.#filteredTasks.sort((a, b) => b.dateInst()-a.dateInst());
-        this.#tasks.sort((a, b) => b.dateInst()-a.dateInst());
+        this.#filteredTasks.sort((a, b) => b.dateInst - a.dateInst);
+        this.#tasks.sort((a, b) => b.dateInst - a.dateInst);
     }
 
     sortByName() {
-        this.#filteredTasks.sort((a, b) =>{ 
+        this.#filteredTasks.sort((a, b) => {
             if (a.name < b.name) {
                 return -1;
-              }
-              if (a.name > b.name) {
+            }
+            if (a.name > b.name) {
                 return 1;
-              }
-              return 0;
+            }
+            return 0;
         });
 
-        this.#tasks.sort((a, b) =>{ 
+        this.#tasks.sort((a, b) => {
             if (a.name < b.name) {
                 return -1;
-              }
-              if (a.name > b.name) {
+            }
+            if (a.name > b.name) {
                 return 1;
-              }
-              return 0;
+            }
+            return 0;
         });
     }
 
-    filterForDoneTasks(){
-        this.#filteredTasks = this.#tasks.filter((task)=> task.isDone);
+    deleteTask(id){
+        const task=this.#tasks.find((task)=>task.id===id);
+        const index=this.#tasks.indexOf(task);
+
+        this.#tasks.splice(index,1);
+        this.#filteredTasks.splice(index,1);
     }
 
-    filterForUndoneTasks(){
-        this.#filteredTasks = this.#tasks.filter((task)=> !task.isDone);
+    filterForDoneTasks() {
+        this.#filteredTasks = this.#tasks.filter((task) => task.isDone);
+        this.#filterType = 'done';
     }
 
-    unfilterTasks(){
+    filterForUndoneTasks() {
+        this.#filteredTasks = this.#tasks.filter((task) => !task.isDone);
+        this.#filterType = 'undone';
+    }
+
+    unfilterTasks() {
         this.#filteredTasks = [...this.#tasks];
-    }
-
-    get tasks() {
-        return this.#tasks;
+        this.#filterType = 'all';
     }
 
     removeAllElementsInList(tasksList) {
@@ -111,33 +132,97 @@ class TasksList {
         }
     }
 
-    appendList(){
-        const tasksList=document.querySelector('#tasks-list');
+    appendList() {
+        const tasksList = document.querySelector('#tasks-list');
         this.removeAllElementsInList(tasksList);
-        for(const task of this.#filteredTasks){
-            const li=document.createElement('li');
-            li.id=task.id;
+        for (const task of this.#filteredTasks) {
+            const li = document.createElement('li');
+            li.id = task.id;
 
-            const spanName=document.createElement('span');
-            spanName.textContent=task.name;
+            const spanName = document.createElement('span');
+            spanName.textContent = task.name;
 
-            const spanStatus=document.createElement('span');
-            spanStatus.textContent=`Status: ${isDone?'Done':'Undone'}`;
+            const spanStatus = document.createElement('span');
+            spanStatus.textContent = `Status: ${task.isDone ? 'Done' : 'Undone'}`;
 
-            const statusCheckbox=document.createElement('input');
-            statusCheckbox.setAttribute('type','checkbox');
-            statusCheckbox.checked=isDone;
+            const statusCheckbox = document.createElement('input');
+            statusCheckbox.className = 'status-checkbox';
+            statusCheckbox.setAttribute('type', 'checkbox');
+            statusCheckbox.checked = task.isDone;
 
-            const editBtn=document.createElement('button');
-            editBtn.className='edit-button';
-            editBtn.textContent='Edit';
 
-            const deleteBtn=document.createElement('button');
-            deleteBtn.className='delete-button';
-            deleteBtn.textContent='Delete';
+            const editBtn = document.createElement('button');
+            editBtn.className = 'edit-button';
+            editBtn.textContent = 'Edit';
 
-            li.append(spanName,spanStatus,statusCheckbox,editBtn,deleteBtn);
+            const deleteBtn = document.createElement('button');
+            deleteBtn.className = 'delete-button';
+            deleteBtn.textContent = 'Delete';
+
+            li.append(spanName, spanStatus, statusCheckbox, editBtn, deleteBtn);
             tasksList.append(li);
         }
     }
+
+    updateList() {
+        const sortType = document.querySelector('#sort-by').value;
+        switch (this.#filterType) {
+            case 'all':
+                this.unfilterTasks();
+                break;
+            case 'done':
+                this.filterForDoneTasks();
+                break;
+            case 'undone':
+                this.filterForUndoneTasks();
+                break;
+        }
+        switch (sortType) {
+            case 'date':
+                this.sortByDate();
+                break;
+            case 'name':
+                this.sortByName();
+                break;
+        }
+        this.appendList();
+    }
+
+    get tasks() {
+        return this.#tasks;
+    }
 }
+
+function updateAll(){
+    tasksList.updateList();
+    deleteButtons = document.querySelectorAll('.delete-button');
+    console.log(deleteButtons);
+    for(let i=0; i<deleteButtons.length;++i){
+        deleteButtons[i].addEventListener('click', (e)=>{
+            console.log('21313');
+            const id = deleteButtons[i].closest('li').id;
+            tasksList.deleteTask(id);
+            updateAll();
+        });
+    }
+}
+
+const form = document.querySelector('#add-task-form');
+const tasksList = localStorage.getItem('tasksList') === null ? new TasksList() : localStorage.getItem('tasksList');
+let deleteButtons = document.querySelectorAll('.delete-button');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputTaskName = document.querySelector('#input-task-name');
+    const inputTaskDescription = document.querySelector('#input-task-description');
+
+    const task = new Task(inputTaskName.value, inputTaskDescription.value);
+    tasksList.addTasks(task);
+
+    updateAll();
+    localStorage.setItem('tasksList', (new Task('dsasdfsf', 'adfasfdaf')).toJson());
+    console.log(JSON.parse((new Task('dsasdfsf', 'adfasfdaf')).toJson()));
+});
+
+
+
